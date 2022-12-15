68f247c36df037b84361818d2bb4db4ffb8e005f
Gropp <fgropp@gmail.com>
2022-12-15 13:00:01 -0300

    link detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ function loadMorePokemons(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
             <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
+                <span class="number"><a class="link" href="#">#${pokemon.number}</a></span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
